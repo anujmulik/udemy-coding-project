d939821406c39a3ded5eea5ca58af7e6cd32b901
Anuj Mulik <anuj9306@gmail.com>
2020-03-08 14:00:19 -0700

    Initial setup

---
+++ src/main/java/com/codingchallenge/quizlet/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.codingchallenge.quizlet.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.codingchallenge.quizlet.entity.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/api/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
---
+++ src/test/java/com/codingchallenge/quizlet/QuizletApplicationTests.java
@@ -0,0 +1,10 @@
+package com.codingchallenge.quizlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizletApplicationTests {
+
+
+}
